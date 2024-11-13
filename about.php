95ef090ff86870979ad2fbddfcb87b305a144f0c
Dinuka Sewwandi <86360286+dinukasewwandi@users.noreply.github.com>
2024-11-13 11:34:12 +0530

    Add files via upload

---
+++ about.php
@@ -0,0 +1,192 @@
+<?php
+/*
+ * Template Name: About Us
+ 
+ */
+?>
+<?php
+ get_header();
+
+  ?>
+
+  <?php
+  
+get_template_part('banner');
+  ?>
+
+   <!-- about section 1  -->
+   <div class="section" id="absec-1">
+        <div class="container">
+            <div class="absec-1-heading text-center pb-3">
+                <h2 class="ab-heading py-3">About us</h2>
+                <span class="pt-4">"We are header-mail for the best solutions for your electrical & power engireening
+                    needs"</span>
+            </div>
+            <div class="absec-1-content pt-5">
+                <div class="row">
+                    <div class="col-sm-12 col-md-6 col-lg-6">
+                        <div class="img-sec">
+                            <img class="img-fluid img-1" src="<?php echo get_template_directory_uri(); ?>/inc/img/absec-img1.jpg" alt="">
+                            <img class="img-fluid img-2" src="<?php echo get_template_directory_uri(); ?>/inc/img/absec-img2.jpg" alt="">
+                        </div>
+                    </div>
+                    <div class="col-sm-12 col-md-6 col-lg-6">
+                        <div class="content-sec">
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa praesentium, necessitatibus
+                                quasi minima alias vel sapiente dolorum aperiam dignissimos adipisci laborum eos
+                                consequatur libero maxime a quo corrupti reprehenderit corporis!</p>
+                            <h5 class="py-3">
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                            </h5>
+                            <table class="absec-t-table mb-4">
+                                <tr>
+                                    <td class="img-td">
+                                        <img class="img-fluid" src="<?php echo get_template_directory_uri(); ?>/inc/img/list-icon2.jpg" alt="">
+                                    </td>
+                                    <td class="sec-td">
+                                        <span>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repudiandae
+                                            quisquam quis</span>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td class="img-td">
+                                        <img class="img-fluid" src="<?php echo get_template_directory_uri(); ?>/inc/img/list-icon2.jpg" alt="">
+                                    </td>
+                                    <td class="sec-td">
+                                        <span>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repudiandae
+                                            quisquam quis</span>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td class="img-td">
+                                        <img class="img-fluid" src="<?php echo get_template_directory_uri(); ?>/inc/img/list-icon2.jpg" alt="">
+                                    </td>
+                                    <td class="sec-td">
+                                        <span>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repudiandae
+                                            quisquam quis</span>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td class="img-td">
+                                        <img class="img-fluid" src="<?php echo get_template_directory_uri(); ?>/inc/img/list-icon2.jpg" alt="">
+                                    </td>
+                                    <td class="sec-td">
+                                        <span>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repudiandae
+                                            quisquam quis</span>
+                                    </td>
+                                </tr>
+                            </table>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sint maiores aut esse corporis
+                                tenetur eligendi molestiae cum quod magni? Est corporis obcaecati minima ad similique
+                                sunt quaerat vitae soluta quam?</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- about section 2  -->
+    <div class="section bg-blue" id="absec-2">
+        <div class="container">
+            <div class="text-center">
+                <h2 class="ab-heading py3">
+                    Brief About Company
+                </h2>
+                <p class="absec-2-content">
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reprehenderit, quas. Molestias laboriosam
+                    pariatur, ea veritatis consequuntur natus, suscipit voluptatibus blanditiis maiores quaerat, odit
+                    aliquam veniam. At officia corporis velit recusandae.
+                </p>
+            </div>
+        </div>
+    </div>
+    <!-- about vision mission sec -->
+    <div class="section" id="vm-sec">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-6 col-md-6 col-lg-6">
+                    <div class="vm-content text-center">
+                        <div class="vm-img">
+                            <img class="img-fluid" src="<?php echo get_template_directory_uri(); ?>/inc/img/sample.png" alt="">
+                        </div>
+                        <div class="vm-title">
+                            <h1>Vision</h1>
+                        </div>
+                        <div class="vm-caption">
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto nihil modi illo
+                                voluptas
+                                vitae quibusdam iure omnis obcaecati ratione, in facilis sapiente nobis tenetur officiis
+                                facere consectetur numquam delectus sint?</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-md-6 col-lg-6">
+                    <div class="vm-content text-center">
+                        <div class="vm-img">
+                            <img class="img-fluid" src="<?php echo get_template_directory_uri(); ?>/inc/img/sample.png" alt="">
+                        </div>
+                        <div class="vm-title">
+                            <h1>Mission</h1>
+                        </div>
+                        <div class="vm-caption">
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto nihil modi illo
+                                voluptas
+                                vitae quibusdam iure omnis obcaecati ratione, in facilis sapiente nobis tenetur officiis
+                                facere consectetur numquam delectus sint?</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Message section -->
+    <div class="section" id="msg">
+        <div class="container">
+            <table class="msg-table">
+                <tr class="tr-1">
+                    <td class="cap-td">
+                        <h1 class="pb-3">MD's Message</h1>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio accusamus consectetur
+                            pariatur ab velit aperiam veniam, deleniti omnis sequi dolorem doloribus eum? Qui deleniti
+                            recusandae ullam obcaecati consequatur. In, nihil!</p>
+                    </td>
+                    <td class="img-td">
+                        <img class="img-fluid" src="<?php echo get_template_directory_uri(); ?>/inc/img/absec-img2.jpg" alt="">
+                    </td>
+                </tr>
+                <tr class="tr-2">
+                    <td class="img-td">
+                        <img class="img-fluid" src="<?php echo get_template_directory_uri(); ?>/inc/img/absec-img2.jpg" alt="">
+                    </td>
+                    <td class="cap-td">
+                        <h1 class="pb-3">CEO's Message</h1>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio accusamus consectetur
+                            pariatur ab velit aperiam veniam, deleniti omnis sequi dolorem doloribus eum? Qui deleniti
+                            recusandae ullam obcaecati consequatur. In, nihil!</p>
+                    </td>
+
+                </tr>
+            </table>
+        </div>
+    </div>
+    <!-- View more banner section -->
+    <div class="section" id="vmore">
+        <div class="container text-center">
+            <h2 class="vmore_text pb-4">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit,
+            </h2>
+            <a class="btn btn-light" href="about.html">View More</a>
+        </div>
+    </div>
+    <!-- map section -->
+    <div class="section" id="map">
+        <div class="container">
+            <img class="img-fluid" src="<?php echo get_template_directory_uri(); ?>/inc/img/map.png" alt="">
+        </div>
+    </div>
+    <!-- Body Content End-->
+
+<?php
+get_footer();
+  ?>
+  
